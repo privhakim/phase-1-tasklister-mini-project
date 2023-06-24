7ecdb1bcbbe66d5422c65a3c3516044e5ee704d0
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-06-24 12:03:27 +0300

    done work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const taskInput = document.querySelector('#new-task-description');
+  const taskDescription = taskInput.value;
+
+  if (taskDescription.trim() !== '') {
+    const listItem = document.createElement('li');
+    listItem.textContent = taskDescription;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('delete-btn');
+    listItem.appendChild(deleteButton);
+
+    taskList.appendChild(listItem);
+
+    taskInput.value = '';
+  }
+});
+
+taskList.addEventListener('click', function(event) {
+  if (event.target.classList.contains('delete-btn')) {
+    const listItem = event.target.parentElement;
+    taskList.removeChild(listItem);
+  }
+});
